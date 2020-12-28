b684a19434292d1ff06473032d96891fd7e67baf
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2020-12-29 03:00:55 +0500

    Create RememberCards model and registered in admin panel

--- remembers/models.py
+++ remembers/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class RememberCards(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.location}: {self.notes[:20]}..."
