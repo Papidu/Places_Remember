8d966867757dd0f15af42e16dc9c2e275f72dd78
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2020-12-31 16:50:18 +0500

    add detiels for postgres db

--- Places_Remember/settings.py
+++ Places_Remember/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'Places_Remember.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'remembers_db',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
