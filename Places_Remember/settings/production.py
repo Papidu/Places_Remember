542928a783fdf42eb1321474a852298e4272dee7
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2021-01-07 19:42:14 +0500

    fix db

--- Places_Remember/settings/production.py
+++ Places_Remember/settings/production.py
@@ -105,7 +105,7 @@ DATABASES = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 if os.getenv('DYNO'):
