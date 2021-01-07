6597d48bdce51881276b0c1d900e585099e5f26a
Vladimir Podprajtov <vladimirpodpratov@gmail.com>
2021-01-08 01:52:44 +0500

    fix db 6

--- Places_Remember/settings/base.py
+++ Places_Remember/settings/base.py
@@ -110,7 +110,8 @@ print(DATABASES['default'])
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-print("------------\n" + DATABASES['default'] + "------------------------")
+print(DATABASES['default'])
+print("------------------------------------")
 if os.getenv('DYNO'):
     GDAL_LIBRARY_PATH = os.path.expandvars(os.getenv('GDAL_LIBRARY_PATH'))
     GEOS_LIBRARY_PATH = os.path.expandvars(os.getenv('GEOS_LIBRARY_PATH'))
